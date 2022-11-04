64c7e3efcd57ae3874f3630edd02d116a7c80b34
Gian Ortega <gianfrancoodp@gmail.com>
2022-11-04 11:27:19 -0300

    Fixed Term Deposit Entity has changed

--- src/main/java/com/alkemy/wallet/model/FixedTermDeposit.java
+++ src/main/java/com/alkemy/wallet/model/FixedTermDeposit.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "fixed_term_deposits")
+@Table(name = "FIXED_TERM_DEPOSITS")
 @Data
 public class FixedTermDeposit {
 
