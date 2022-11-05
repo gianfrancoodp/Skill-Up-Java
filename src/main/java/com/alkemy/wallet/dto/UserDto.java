0b0a00d76efd932bb46afac604494a48c85cf998
Denise <denise_colotta@hotmail.com>
2022-11-05 14:55:14 -0300

    last commit

--- src/main/java/com/alkemy/wallet/dto/UserDto.java
+++ src/main/java/com/alkemy/wallet/dto/UserDto.java
@@ -3,7 +3,7 @@ package com.alkemy.wallet.dto;
 import lombok.Data;
 
 @Data
-public class UserDto {
+public class UserDTO {
     private Long id;
     private String firstName;
     private String lastName;
