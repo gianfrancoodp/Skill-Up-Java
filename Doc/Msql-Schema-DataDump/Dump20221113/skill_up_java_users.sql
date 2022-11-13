c1ad8caaaf8154534a58fa5d1ba006bc0df4cae7
leonmotard <leandro.jj.silva@gmail.com>
2022-11-13 14:54:09 -0300

    Correction of paged request for users, transactions and accounts.

---
+++ Doc/Msql-Schema-DataDump/Dump20221113/skill_up_java_users.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: skill_up_java
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'2022-11-12 21:39:31.171000',_binary '\0','prueba@prueba.com','Alberto','Silva','$2a$10$p5gWhthqEF03L9wRlvoCb.tObSnk8zcuDgQDQ1xNpwOjc51FgUDyy',1,'2022-11-12 21:39:31.171000'),(3,'2022-11-12 21:45:49.310000',_binary '\0','ana@prueba.com','Ana','Cuesta','$2a$10$yw9XpNNgTACRYbR0UE79yOH.BnzveOmXt2RPmw3eHxNItSAhBZBqe',2,'2022-11-12 21:45:49.310000'),(4,'2022-11-12 21:48:50.120000',_binary '\0','debo@prueba.com','Debora','Cuesta','$2a$10$19AXcxkULDDuVK5lABwnO.yvoWCmLvYwtTQQw6jFZwcWfXGQTgn.2',2,'2022-11-12 21:48:50.120000'),(5,'2022-11-12 21:50:44.741000',_binary '\0','jose@prueba.com','Jose','Vergara','$2a$10$2AB.o8s3TNk6vJu2qsU6hu1bYUvX.wwXFNY1bSfZTUVXaLy8bEPJC',1,'2022-11-12 21:50:44.742000'),(6,'2022-11-12 21:56:24.388000',_binary '\0','js@prueba.com','Jose','Vergara','$2a$10$nzTqELsTa9tOPtBbytyTYuhtGdjmQhzFb1vc9jw0PqE6IIMke8v1q',2,'2022-11-12 21:56:24.388000'),(7,'2022-11-12 21:57:26.307000',_binary '\0','an@prueba.com','Ana','Vergara','$2a$10$1qhi2uEi2nL8QWGARWUH1et/pp7.4nd4Ow438Uc2UWkPN1d/hbDvy',2,'2022-11-12 21:57:26.307000'),(8,'2022-11-12 21:59:04.280000',_binary '\0','gabriela@prueba.com','Gabriela','Vergara','$2a$10$1lPX0nEturhTsi.TY/lVQOmo2W/Vw6DXVJhCRH5a1OfjtKgobdfji',2,'2022-11-12 21:59:04.280000'),(9,'2022-11-12 21:59:29.532000',_binary '\0','gonzalo@prueba.com','Gonzalo','Rodriguez','$2a$10$3WNnZN.p9wzLnmSmR11Ww.bXrVdYHZwsybUrzLw0EmRXDi7iwu4f.',2,'2022-11-12 21:59:29.532000'),(10,'2022-11-12 22:00:06.173000',_binary '\0','emilio@prueba.com','Emilio','Rodriguez','$2a$10$PQnO0evyJa56ooJu35sbCeEqdVIbKNu3vuyKt1IperUPyVzpomkZi',2,'2022-11-12 22:00:06.173000'),(11,'2022-11-12 22:00:39.873000',_binary '\0','monica@prueba.com','Monica','Rodriguez','$2a$10$GsXj4xm0l.dsCQIQ41rHRutsKLVSYc45qRT33JAJdB/wrlJ0TyRV2',2,'2022-11-12 22:00:39.873000'),(12,'2022-11-13 14:10:54.291000',_binary '\0','junior@prueba.com','Junior','Rodriguez','$2a$10$2dA9/8Hbv1pHh/HOOvEnwe.2xHf/IcXCnPpjqYw9bgGhWJfwgohsi',2,'2022-11-13 14:10:54.291000'),(13,'2022-11-13 14:11:10.813000',_binary '\0','julia@prueba.com','Julia','Rodriguez','$2a$10$wIZRpMaXmiZ0SNY2WeSPdeM76SHvmfCnw.jT4cAsF8.r2GN2cKZpS',2,'2022-11-13 14:11:10.813000'),(14,'2022-11-13 14:11:30.397000',_binary '\0','eduardo@prueba.com','Eduardo','Rodriguez','$2a$10$S8v60TVQ74VEhEsjzKqCHecsBPp9G.BNMrTyOwbTcDaRmzEVkKy/m',2,'2022-11-13 14:11:30.398000');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-13 14:49:51
