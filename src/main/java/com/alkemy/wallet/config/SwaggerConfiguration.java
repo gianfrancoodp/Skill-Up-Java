2302cd3201ebe85b3c28c5a84b0b8f33ca78ceae
aldogayala14 <aldo.g.ayala@gmail.com>
2022-11-13 21:04:04 -0300

    Add Swagger config and path in SpringSecurity

--- src/main/java/com/alkemy/wallet/config/SwaggerConfiguration.java
+++ src/main/java/com/alkemy/wallet/config/SwaggerConfiguration.java
@@ -2,7 +2,10 @@ package com.alkemy.wallet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,11 +17,25 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket productApi() {
+    public Docket configApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.alkemy.wallet"))
-                .paths(regex("/api/docs"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
+
+
+    /**
+     * Method to set swagger info
+     *
+     * @return ApiInfoBuilder
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Wallet").description("API developed for virtual wallet for Alkemy Java SkillUp").version("1.0").build();
+    }
+
+
 }
+
